7573b0e1f385f5b9adb7dd001576e3246cd56c13
Kenneth Velazquez <kenny.arcelay@gmail.com>
2023-05-20 22:55:58 -0500

    deploy minor fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: "vite build --base=./",
+  base: "/kenbnb/",
   plugins: [react()],
 })
